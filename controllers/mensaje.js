1f9ec463935e9e7c37c457f48b6c133e111f6afd
Enrique Barra Arias <enrique.barra.arias@gmail.com>
2021-03-23 17:43:18 +0100

    Update mensaje.js

--- controllers/mensaje.js
+++ controllers/mensaje.js
@@ -5,6 +5,5 @@ const Mensaje = require('../models/mensaje');
 //IMPORTANTE: recuerda escribir el username con la primera letra mayúscula
 //y los devuelve en la variable return como un array
 exports.filterMensajes = async function () {
-    let result = await Mensaje.find({"user.username": "Cocodrilo"});
-    return result;
+    
 }
